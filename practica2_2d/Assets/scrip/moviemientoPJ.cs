2ee16ab84cd5efd52faff89e87968e3d3a94183c
jobrutis <Joseantoniopadillotoril@gmail.com>
2024-02-15 18:40:29 +0100

    animacion
    animacion moneda, platano y enemigo

--- practica2_2d/Assets/scrip/moviemientoPJ.cs
+++ practica2_2d/Assets/scrip/moviemientoPJ.cs
@@ -8,75 +8,87 @@ public class moviemientoPJ : MonoBehaviour
     public float velocidad;
     Rigidbody2D rigidBody;
     private float inputMovement;
-    public bool mirandoDerecha= true;
+    public bool mirandoDerecha = true;
     public float velSalto;
     public LayerMask surfacelayer;
-    //estas dos variables es pa arreglar el slato
+    // Estas dos variables son para arreglar el salto
     public bool enelsuelo;
     BoxCollider2D boxCollider;
-    //animacion correr
+    // Animación correr
     public bool correr;
     private Animator animator;
 
-    //Doble salto
-    public int dobleS;
+    // Doble salto
+    public int dobleS = 2; // Puedes ajustar el valor según tus necesidades
+    private int saltosRealizados = 0;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
-        animator= GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
-
-    bool Comprobarsuelo() {
-
+    bool ComprobarSuelo()
+    {
         RaycastHit2D raycastHit = Physics2D.BoxCast(
-                                        boxCollider.bounds.center, //Origen de la caja
-                                        new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), //Tamaño
-                                        0f, //Ángulo
-                                        Vector2.down, //Direccion hacia la que va la caja
-                                        0.2f, //Distancia a la que aparece la caja
-                                        surfacelayer//Layer mask
-                                        );
-        return raycastHit.collider != null; //Devuelvo un valor siempre que no sea nulo
-
+                                        boxCollider.bounds.center,
+                                        new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y),
+                                        0f,
+                                        Vector2.down,
+                                        0.2f,
+                                        surfacelayer
+                                    );
+        return raycastHit.collider != null;
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
         ProcessingMovement();
         Salto();
-        enelsuelo = Comprobarsuelo();
-
+        enelsuelo = ComprobarSuelo();
     }
-   private void ProcessingMovement() 
+
+    private void ProcessingMovement()
     {
-        
         inputMovement = Input.GetAxis("Horizontal");
-        correr = inputMovement != 0 ?  true : false ;
+        correr = inputMovement != 0 ? true : false;
         animator.SetBool("correr", correr);
         rigidBody.velocity = new Vector2(inputMovement * velocidad, rigidBody.velocity.y);
-        Pjorientacion(inputMovement);
+        PjOrientacion(inputMovement);
     }
 
-    private void Pjorientacion(float inputMovement)
+    private void PjOrientacion(float inputMovement)
     {
         if ((mirandoDerecha && inputMovement < 0) || (!mirandoDerecha && inputMovement > 0))
         {
             mirandoDerecha = !mirandoDerecha;
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
         }
-
     }
-    private void Salto() { 
-        if(Input.GetKeyDown(KeyCode.Space) && enelsuelo)
+
+    private void Salto()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidBody.AddForce(Vector2.up * velSalto, ForceMode2D.Impulse);
+            if (enelsuelo)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
+                rigidBody.AddForce(Vector2.up * velSalto, ForceMode2D.Impulse);
+                saltosRealizados = 1; // Primer salto realizado
+            }
+            else if (saltosRealizados < dobleS)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
+                rigidBody.AddForce(Vector2.up * velSalto, ForceMode2D.Impulse);
+                saltosRealizados++;
+            }
+        }
+        // Restablecer la cantidad de saltos realizados cuando toca el suelo
+        if (enelsuelo)
+        {
+            saltosRealizados = 0;
         }
     }
-
 }
