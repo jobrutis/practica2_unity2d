c88a18bc37cd061e7c067711af639cbf1b48ba48
jobrutis <Joseantoniopadillotoril@gmail.com>
2024-02-19 20:14:18 +0100

    puntos

--- practica2_2d/Assets/scrip/score.cs
+++ practica2_2d/Assets/scrip/score.cs
@@ -7,8 +7,10 @@ using UnityEngine;
 public class score : MonoBehaviour
 {
    
-    public int totalscore;
+   public int totalscore;
    public int totalscoreT;
+    public TMP_Text text;
+    public TMP_Text trofeo;
     void Start()
     {
        
@@ -17,6 +19,7 @@ public class score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
- 
+        text.text = ""+totalscore;
+        trofeo.text = "" + totalscoreT;
     }
 }
