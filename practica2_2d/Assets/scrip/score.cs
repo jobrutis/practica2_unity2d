d610ad220d38bd1754b94644397b59f63e9e27b2
jobrutis <Joseantoniopadillotoril@gmail.com>
2024-02-20 20:16:17 +0100

    puntaje
    puntaje y arreglo de animacion

--- practica2_2d/Assets/scrip/score.cs
+++ practica2_2d/Assets/scrip/score.cs
@@ -7,19 +7,15 @@ using UnityEngine;
 public class score : MonoBehaviour
 {
    
-   public int totalscore;
-   public int totalscoreT;
-    public TMP_Text text;
-    public TMP_Text trofeo;
-    void Start()
-    {
-       
-    }
+   public int totalscore=0;
+   public int totalscoreT=0;
+   public TMPro.TMP_Text text;
+    public TMPro.TMP_Text trofeo;
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        text.text = ""+totalscore;
-        trofeo.text = "" + totalscoreT;
+        text.text = " " + totalscore;
+        trofeo.text = " " + totalscoreT;
     }
 }
